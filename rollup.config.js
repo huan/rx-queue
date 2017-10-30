74428e1d93ab9eccce4e49706619fc13c5277cc5
Huan LI <zixia@zixia.net>
2017-10-30 14:44:50 +0800

    add rollup for publish npm

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import json from 'rollup-plugin-json'
+
+export default {
+  entry: 'dist/rx-queue.js',
+  dest: 'bundles/rx-queue.es6.umd.js',
+  sourceMap: true,
+  format: 'umd',
+  moduleName: 'window',
+  banner: '/* rx-queue version ' + require('./package.json').version + ' */',
+  footer: '/* https://github.com/zixia */',
+  plugins: [
+    json({
+      // All JSON files will be parsed by default,
+      // but you can also specifically include/exclude files
+      // include: 'node_modules/**',  // Default: undefined
+      // exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],  // Default: undefined
+      preferConst: true, // Default: false
+    })
+  ]
+}
--- tests/fixtures/smoke-testing.js
+++ tests/fixtures/smoke-testing.js
@@ -1,6 +1,6 @@
 const { RxQueue } = require('rx-queue')
 
 const queue = new RxQueue()
-queue.destroy()
+console.log(`RxQueue v${queue.version()}`)
 
 console.log('Smoke Testing PASSED!')
