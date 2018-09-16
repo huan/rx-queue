373f3677d211c92d4ab2fd47f730429fd12bfef0
Huan LI <zixia@zixia.net>
2018-09-17 00:36:32 +0800

    code clean

--- src/delay-queue.spec.ts
+++ src/delay-queue.spec.ts
@@ -3,7 +3,6 @@
 // tslint:disable:no-shadowed-variable
 import * as test  from 'blue-tape'
 import * as sinon from 'sinon'
-// const sinonTest   = require('sinon-test')(sinon)
 
 import DelayQueue from './delay-queue'
 
